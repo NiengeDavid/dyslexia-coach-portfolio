2d221c201d1fd621d6c1fbc636c76f1d84c132db
Anyanwu-victory <118054739+Anyanwu-victory@users.noreply.github.com>
2024-02-29 12:41:29 +0000

    Testing
    Tried the testing and showing the instructor.

--- dyslexia_frontend/src/components/About.jsx
+++ dyslexia_frontend/src/components/About.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import background from '../Assets/pexels-pixabay-60013.jpg'
 
 const About = () => {
   return (
     <div className='font-bold text-center text-red-500 text-2xl py-10'>
         About
+
+        <div className='flex font-bold text-red-500 text-5xl'>
+                Felix has an headache because of dyslexia
+            
+            <span> <img src={background} alt="" className='flex' /></span>
+        </div>
     </div>
   )
 }
