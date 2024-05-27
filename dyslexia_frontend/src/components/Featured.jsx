5d9c8e28c957db1ef9b13c6edae2b95a3c978b9b
NiengeDavid <97910242+NiengeDavid@users.noreply.github.com>
2024-05-27 17:25:48 +0100

    grid

--- dyslexia_frontend/src/components/Featured.jsx
+++ dyslexia_frontend/src/components/Featured.jsx
@@ -20,7 +20,7 @@ const Featured = ({ data, title }) => {
         {heading ? (
           <div className="py-8 lg:py-16 mx-auto max-w-screen-xl px-4">
             <h2 className="mb-8 lg:mb-16 text-3xl font-extrabold tracking-tight font-[Segoe UI] leading-tight text-center text-[#006273] md:text-4xl">{heading[0].title}</h2>
-            <div className="grid grid-cols-2 gap-8 text-[#73A4A0] md:gap-12 md:grid-cols-3 lg:grid-cols-6">
+            <div className="grid grid-cols-2 gap-8 text-[#73A4A0] md:gap-12 md:grid-cols-4 lg:grid-cols-4">
                 {logos.map((item, index) => (
                   <a key={index} href={item.link} className="flex justify-center items-center transition-transform hover:duration-300 hover:transform hover:translate-y-1">
                     <img src={item.logo.asset.url} alt={item.alt} />                        
